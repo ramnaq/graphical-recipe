25c438f82638f65f5d3cd164216671a96bacbd8b
Ramna Sidharta <ramna.thors@gmail.com>
2019-03-21 17:39:48 -0300

    c_handlers documentation: correspondence to signals and elements

--- src/c_handlers.hpp
+++ src/c_handlers.hpp
@@ -6,6 +6,7 @@
  * @see g_signal_connect (GTK)
  */
 
+//! Connected to signal "draw" of element "drawAreaViewPort".
 extern "C" G_MODULE_EXPORT void draw(GtkWidget *widget, cairo_t *cr,  gpointer   data){
   try {
     controller->draw(cr);
@@ -14,6 +15,7 @@ extern "C" G_MODULE_EXPORT void draw(GtkWidget *widget, cairo_t *cr,  gpointer  
   }
 }
 
+//! Connected to signal "create_surface" of element "drawAreaViewPort".
 extern "C" G_MODULE_EXPORT void create_surface(GtkWidget *widget){
   try {
     controller->create_surface(widget);
@@ -22,6 +24,7 @@ extern "C" G_MODULE_EXPORT void create_surface(GtkWidget *widget){
   }
 }
 
+//! Connected to signal "clicked" of element "btnAddObject".
 extern "C" G_MODULE_EXPORT void on_btn_clicked_open_window() {
   try {
     controller->openNewObjectWindow();
@@ -30,6 +33,7 @@ extern "C" G_MODULE_EXPORT void on_btn_clicked_open_window() {
   }
 }
 
+//! Connected to signal "clicked" of element "btnOk".
 extern "C" G_MODULE_EXPORT void on_btn_clicked_create_object() {
   try {
     controller->createObject();
@@ -38,6 +42,7 @@ extern "C" G_MODULE_EXPORT void on_btn_clicked_create_object() {
   }
 }
 
+//! Connected to signal "clicked" of element "btnAddCoordenada".
 extern "C" G_MODULE_EXPORT void on_btnAddCoordenada_addCoordenada() {
   try {
     controller->addNewLineForPolygon();
@@ -46,6 +51,7 @@ extern "C" G_MODULE_EXPORT void on_btnAddCoordenada_addCoordenada() {
   }
 }
 
+//! Connected to signal "clicked" of element "btnRemoveObject".
 extern "C" G_MODULE_EXPORT void on_btn_clicked_remove_object() {
   try {
     controller->removeFromList();
@@ -54,6 +60,7 @@ extern "C" G_MODULE_EXPORT void on_btn_clicked_remove_object() {
   }
 }
 
+//! Connected to signal "clicked" of element "btnRemoverCoordenada".
 extern "C" G_MODULE_EXPORT void on_btnRemoverCoordenada_clicked() {
   try {
     controller->removeFromCoordPolygonList();
@@ -62,6 +69,7 @@ extern "C" G_MODULE_EXPORT void on_btnRemoverCoordenada_clicked() {
   }
 }
 
+//! Connected to signal "configure-event" of element "mainWindow".
 extern "C" G_MODULE_EXPORT void on_mainWindow_configure_event(){
   try {
     controller->initializeWindowViewPort();
@@ -70,6 +78,7 @@ extern "C" G_MODULE_EXPORT void on_mainWindow_configure_event(){
   }
 }
 
+//! Connected to signal "clicked" of element "btnZoomIn".
 extern "C" G_MODULE_EXPORT void on_btn_clicked_in(){
   try {
     controller->changeWindow(0);
@@ -78,6 +87,7 @@ extern "C" G_MODULE_EXPORT void on_btn_clicked_in(){
   }
 }
 
+//! Connected to signal "clicked" of element "btnZoomOut".
 extern "C" G_MODULE_EXPORT void on_btn_clicked_out(){
   try {
     controller->changeWindow(1);
@@ -86,6 +96,7 @@ extern "C" G_MODULE_EXPORT void on_btn_clicked_out(){
   }
 }
 
+//! Connected to signal "clicked" of element "btnDireita".
 extern "C" G_MODULE_EXPORT void on_btn_clicked_dir(){
   try {
     controller->changeWindow(2);
@@ -94,6 +105,7 @@ extern "C" G_MODULE_EXPORT void on_btn_clicked_dir(){
   }
 }
 
+//! Connected to signal "clicked" of element "btnEsquerda".
 extern "C" G_MODULE_EXPORT void on_btn_clicked_esq(){
   try {
     controller->changeWindow(3);
@@ -102,6 +114,7 @@ extern "C" G_MODULE_EXPORT void on_btn_clicked_esq(){
   }
 }
 
+//! Connected to signal "clicked" of element "btnCime".
 extern "C" G_MODULE_EXPORT void on_btn_clicked_cima(){
   try {
     controller->changeWindow(4);
@@ -110,6 +123,7 @@ extern "C" G_MODULE_EXPORT void on_btn_clicked_cima(){
   }
 }
 
+//! Connected to signal "clicked" of element "btnBaixo".
 extern "C" G_MODULE_EXPORT void on_btn_clicked_baixo(){
   try {
     controller->changeWindow(5);
@@ -118,6 +132,7 @@ extern "C" G_MODULE_EXPORT void on_btn_clicked_baixo(){
   }
 }
 
+//! Connected to signal "clicked" of element "btnSupEsq".
 extern "C" G_MODULE_EXPORT void on_btn_clicked_supEsq(){
   try {
     controller->changeWindow(6);
@@ -126,6 +141,7 @@ extern "C" G_MODULE_EXPORT void on_btn_clicked_supEsq(){
   }
 }
 
+//! Connected to signal "clicked" of element "btnSupDir".
 extern "C" G_MODULE_EXPORT void on_btn_clicked_supDir(){
   try {
     controller->changeWindow(7);
@@ -134,6 +150,7 @@ extern "C" G_MODULE_EXPORT void on_btn_clicked_supDir(){
   }
 }
 
+//! Connected to signal "clicked" of element "btnInfEsq".
 extern "C" G_MODULE_EXPORT void on_btn_clicked_infEsq(){
   try {
     controller->changeWindow(8);
@@ -142,6 +159,7 @@ extern "C" G_MODULE_EXPORT void on_btn_clicked_infEsq(){
   }
 }
 
+//! Connected to signal "clicked" of element "btnInfDir".
 extern "C" G_MODULE_EXPORT void on_btn_clicked_infDir(){
   try {
     controller->changeWindow(9);
