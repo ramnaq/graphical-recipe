98d23c7b8762794c2b5d9d2222024521689377b2
Ramna Sidharta <ramna.thors@gmail.com>
2019-04-07 13:28:47 -0300

    Better dealing with Polygon runtime_error

--- src/controller.hpp
+++ src/controller.hpp
@@ -2,6 +2,8 @@
 #define CONTROLLER_HPP
 
 #include <iostream>
+#include <stdexcept>
+
 #include "enum.hpp"
 #include "view.hpp"
 #include "point.hpp"
@@ -74,11 +76,15 @@ public:
       case POLYGON: {
         Polygon* polygon;
         try {
+          if (pointsForPolygon.size() < 3) {
+            throw std::runtime_error("Cannot create polygon without at least three points!");
+          }
           polygon = new Polygon(name, pointsForPolygon);
           display.insert(polygon);
           view.newPolygon(polygon);
           pointsForPolygon.clear();
-        } catch(...) {
+        } catch(const std::runtime_error& e) {
+          std::cout << "[ERROR] " << e.what() << std::endl;
           view.logError("Pontos insuficientes para criação de polígono.\n");
         }
         break;
--- src/polygon.hpp
+++ src/polygon.hpp
@@ -11,9 +11,6 @@ class Polygon: public GraphicObject {
 public:
   Polygon(string name, vector<Coordinate*> &polygonCoordinates) :
     GraphicObject(name, POLYGON, polygonCoordinates) {
-      if (polygonCoordinates.size() < 3) {
-        throw std::runtime_error("Cannot create polygon without at least three points!");
-      }
   }
 
 };
